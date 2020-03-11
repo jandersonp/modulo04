3ba520f81a297f9032e8eefb2679bb24e55b7966
Janderson <jandersonpizziallyvilasboas@gmail.com>
2020-03-11 15:22:35 -0300

    chore: Importando Imagens

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 /**Importação do CSS */
 import './App.css'
 
+/**Importando Imagens */
+import profile from './assets/profile.jpg'
+
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return <img width="500" src={profile} />
 }
 
 export default App;
